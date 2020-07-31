eb57bdf9006281c8be8d4434a12f98706cc2fb59
KennethChong <kenneth@neuroleap.com>
2020-07-30 18:06:20 -0700

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Kenneth Chong on 7/30/20.
+//  Copyright © 2020 Kenneth Chong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
